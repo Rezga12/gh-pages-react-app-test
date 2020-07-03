0633c296b959e9ebb0526ece8f1f47703a5dbc58
rezga12 <rmeis16@freeuni.edu.ge>
2020-07-03 17:36:15 +0400

    Updates

---
+++ precache-manifest.c83344bb255a04f68857a09714493436.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c34bd0e0cbaa81c130fcbaade999f3a",
+    "url": "/gh-pages-react-app-test/index.html"
+  },
+  {
+    "revision": "fb39d9778ebe8d4d2eb8",
+    "url": "/gh-pages-react-app-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da25f268304f8c847c75",
+    "url": "/gh-pages-react-app-test/static/js/2.6bf86e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-react-app-test/static/js/2.6bf86e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb39d9778ebe8d4d2eb8",
+    "url": "/gh-pages-react-app-test/static/js/main.26cf6678.chunk.js"
+  },
+  {
+    "revision": "018388eac2238f1e23b2",
+    "url": "/gh-pages-react-app-test/static/js/runtime-main.cbffb1b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
